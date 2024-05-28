7fda206b7d322e9be27441c2cb5c04c38dc3d359
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-05-28 13:38:43 +0300

    Component Event Handlers

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
-<script>
-
-</script>
-
 <template>
+    <h3>{{info}}</h3>
+    <p>{{someInfo}}</p>
 
-  <header>
-    ALALA
-  </header>
-
-  <main>
-
-  </main>
+    <button type="button" @click="userData('Some new')">Send</button>
 </template>
 
 <style scoped>
+  h3 {
+    font-weight: lighter;
+  }
+
+  p {
+    color:red;
+  }
+
 </style>
+<script>
+  export default{
+    data(){
+      return {
+        info:'Title',
+        someInfo: 'Anons of message'
+      }
+    },
+    methods: {
+      userData(word = 'New Test')
+      {
+        this.info = word;
+      }
+
+    }
+  }
+</script>
